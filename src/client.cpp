88e29d50edb11e68e6fbec29c92324653007b4dc
dunajski <michaldunajski2505@gmail.com>
2025-03-23 11:03:29 +0100

    Working example of client-server connection WiP
    Server shall be started first and then run client.cpp
    This examples was made according to this:
    https://github.com/SFML/SFML/blob/master/examples/sockets/TCP.cpp

--- src/client.cpp
+++ src/client.cpp
@@ -4,19 +4,29 @@
 
 int run_client()
 {
+    sf::TcpSocket socket;
+
+    if (const sf::Socket::Status status = socket.connect({192, 168, 0, 123}, 53000); status != sf::Socket::Status::Done)
+    {
+        // TODO improve error handling
+        std::cout << "Connection failed" << std::endl;
+        return 0;
+    }
+
+    std::cout << "Connected" << std::endl;
+    std::array<char, 1024> buffer{};
+    if (std::size_t received = 0; socket.receive(buffer.data(), buffer.size(), received) != sf::Socket::Status::Done)
+        return 0; // TODO improve error handling
+    std::cout << "Message received from the server: " << std::quoted(buffer.data()) << std::endl;
+
+    static constexpr std::string_view client_message = "Hello from client!";
+    if (socket.send(client_message.data(), client_message.size()) != sf::Socket::Status::Done)
+        return 0; // TODO improve error handling
+
+    std::cout << "Message sent to the server: " << std::quoted(client_message.data()) << std::endl;
+
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "SFML TEST 1");
     window.setFramerateLimit(144);
-
-    sf::TcpSocket socket;
-    sf::Socket::Status status = socket.connect({192, 168, 0, 5}, 53000);
-    if (status != sf::Socket::Status::Done)
-    {
-
-    }
-    else
-    {
-        std::cout << "Connected" << std::endl;
-    }
 
     while (window.isOpen())
     {
@@ -32,7 +42,6 @@ int run_client()
         window.clear();
         window.display();
     }
-
 
     return 0;
 }
--- src/server.cpp
+++ src/server.cpp
@@ -8,21 +8,39 @@ int run_server()
     sf::TcpListener listener;
     std::cout << "Listening on port 53000..." << std::endl;
 
+
+    // Retrieve and print the server's IP address
+    sf::IpAddress const server_ip = sf::IpAddress::getLocalAddress().value();
+    std::cout << "Server is running on: " << server_ip.toString() << std::endl;
     // bind the listener to a port
-    if (listener.listen(53000) != sf::Socket::Status::Done)
+    if (listener.listen(53000, server_ip) != sf::Socket::Status::Done)
     {
-        // error...
+        // TODO improve error handling
+        return 0;
     }
 
     // accept a new connection
-    sf::TcpSocket client;
-    if (listener.accept(client) != sf::Socket::Status::Done)
+    sf::TcpSocket socket;
+    if (listener.accept(socket) != sf::Socket::Status::Done)
     {
-        // error...
+        // TODO improve error handling
+        return 0;
     }
 
-    // use "client" to communicate with the connected client,
-    // and continue to accept new connections with the listener
+    std::cout << "Connected: " << socket.getRemoteAddress().value() << std::endl;
+    static constexpr std::string_view server_message = "Hello from server!";
+
+    if (socket.send(server_message.data(), server_message.size()) != sf::Socket::Status::Done)
+        return 0; // TODO improve error handling
+
+    std::cout << "Message sent to the client: " << std::quoted(server_message.data()) << std::endl;
+
+    std::array<char, 1024> buffer{};
+    std::size_t received = 0;
+    if (socket.receive(buffer.data(), buffer.size(), received) != sf::Socket::Status::Done)
+        return 0; // TODO improve error handling
+
+    std::cout << "Message received from the client: " << std::quoted(buffer.data()) << std::endl;
 
     return 0;
 }
